af65248f28a5fd270c40b6a2b490b5c33d4172d5
Arip Slayer <110511365+AripBotz@users.noreply.github.com>
2022-09-13 13:10:02 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Indosat [085802829541]
+│ • Xl  [085940685233]
+│ • Dana  [085802829541]
+│ • Saweria  [https://saweria.co/aripslayer]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6285802829541
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
